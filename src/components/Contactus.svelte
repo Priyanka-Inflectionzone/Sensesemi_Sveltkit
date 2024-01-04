e70641a921cc911943add1caf73da041cf788497
Priyanka-Inflectionzone <priyanka.kale@inflectionzone.com>
2024-01-04 16:31:42 +0530

    Added email service

--- src/components/Contactus.svelte
+++ src/components/Contactus.svelte
@@ -1,14 +1,37 @@
 
-
 <!-- contact us section starts -->
+<script>
+  import { EmailService } from '$lib/email.service.js';
+
+const emailService = new EmailService();
+let formData = {};
+
+async function handleSubmit() {
+  const emailDetails = {
+    EmailTo: formData.email.trim(),
+    Subject: 'New Contact Form Submission',
+    Body: 'Greetings from Sensesemi Technologies. Thank you for contacting us. We will get back to you soon.',
+  };
+
+  try {
+    const result = await emailService.sendEmail(emailDetails, false); // Assuming you want to send HTML email
+    console.log('Email sent:', result);
+    // Handle success, show a success message, or redirect to a thank-you page
+  } catch (error) {
+    console.error('Error sending email:', error);
+    // Handle error, show an error message, or take appropriate action
+  }
+}
+</script>
+<main>
 <div id="contactus" class="flex justify-center mb-12 mt-6 ">
     <h1 class="text-xl text-[#00a8ca] sm:3xl md:text-4xl lg:text-4xl xl:5xl font-bold mt-5 text-center font-poppins leading-relaxed">Contact us</h1>
   </div>
-  
+
   <div class="flex justify-center flex-col md:flex-col lg:flex-row xl:flex-row ">
-  <div class="   xl:w-[363px] xl:h-[480px] p-7 text-white rounded-3xl" style="background: linear-gradient(to bottom, #051e58, #00a8ca); ">
+  <div class="   xl:w-[363px] xl:h-[529px] p-7 text-white rounded-3xl" style="background: linear-gradient(to bottom, #051e58, #00a8ca); ">
     <h1 class="text-2xl  text-left mt-4 font-poppins ">Contact Information</h1>
-    
+
     <p class=" mt-6 mb-6 text-justify text-sm  font-poppins">Fill up the form and our team will get back to you within 24 hours.</p>
     <div class="mt-0 space-y-4">
       <!-- Contact Number1 -->
@@ -16,30 +39,30 @@
         <i class="fas fa-phone text-xl"></i>
         <p class="mt-1 sm:ml-2 font-poppins text-sm" >080 47573057</p>
       </div>
-  <!-- Contact Number2
+  <!-- Contact Number2 -->
   <div class="flex flex-col  sm:flex-row sm:items-center">
     <i class="fas fa-phone text-lg"></i>
     <p class="mt-1 sm:ml-2 font-poppins text-sm">935 3218558</p>
   </div>
-   -->
+
       <!-- Website Link -->
       <div class="flex flex-col md:items-center sm:flex-row sm:items-center">
         <i class="fas fa-globe text-xl"></i>
         <p class="mt-1 sm:ml-2 text-sm"><a href="https://sensesemi.com/home/#" class="text-white hover:text-blue-300 font-poppins">www.sensesemi.com</a></p>
       </div>
-  
+
       <!-- Address -->
       <div class="flex flex-col md:items-center sm:flex-row sm:items-center">
          <i class="fas fa-address-book text-xl"></i>
         <p class="mt-1 sm:ml-2 text-left font-poppins text-sm">Sensesemi Technologies Private Limited No. E40/E41, Ground Floor, 2nd Cross, Syndicate Bank Colony, Arekere Bangalore-560076</p>
       </div>
-  
+
       <!-- Location Link -->
       <div class="flex flex-col md:items-center sm:flex-row sm:items-center">
         <i class="fas fa-map-marker-alt text-xl"></i>
         <p class="mt-1 sm:ml-2 text-sm"><a href="https://www.google.com/maps/place/Almighty+Printing+Solutions/@12.8849418,77.5947082,19z/data=!4m6!3m5!1s0x3bae1531cde6e6eb:0x7a14fb680a111cb6!8m2!3d12.8850081!4d77.5955223!16s%2Fg%2F1q5cctcr7?entry=ttu" class="text-white mb-10 hover:text-blue-300 font-poppins">Google Map</a></p>
       </div>
-  
+
       <!-- Social Links -->
       <div class=" flex justify-center xl:pt-6">
       <a  href="https://facebook.com/"><i  class="fab fa-facebook text-xl"> </i></a>
@@ -49,12 +72,10 @@
     </div>
   </div>
 
-  
 
 <!-- Contact Form -->
 <div class="xl:w-1/2 p-8  ">
-
-  <form class="text-center" method="POST" >
+  <form on:submit|preventDefault={handleSubmit} class="text-center">
 
     <div class="flex flex-row ">
     <div class="mb-4 border-b-2 mr-12  w-80 ">
@@ -89,10 +110,8 @@
         Send Message
       </button>
     </div>
-
   </form>
-
 </div>
 </div>
-  
+</main>
 <!-- contact us section ends -->
